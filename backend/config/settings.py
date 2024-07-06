8a55e64cf34276e2418ce91959ef6d3e00f26163
frshd <frshdpnhi@gmail.com>
2024-07-07 01:29:52 +0330

    :construction: store app installed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,7 +14,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app.store',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
